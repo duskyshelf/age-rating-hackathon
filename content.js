47128bbca6985989c0e9daa029d41986a4e95ee0
DuskyShelf <dupsdale@gmail.com>
2015-10-19 23:20:50 +0200

    added API calls for oopaAPI

--- content.js
+++ content.js
@@ -8,17 +8,29 @@ $(document).ready(function() {
   var locationFinder = function() { navigator.geolocation.getCurrentPosition(function(position) {
         lati = position.coords.latitude;
         longi = position.coords.longitude;
-        console.log(lati);
-        console.log(longi);
-        url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ lati + ',' + longi + '&key=AIzaSyAo8c47NS0NynIW1tzlu3MTHqUakcPzWzk';
+        // url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+ lati + ',' + longi + '&key=AIzaSyAo8c47NS0NynIW1tzlu3MTHqUakcPzWzk';
         bingurl = 'https://dev.virtualearth.net/REST/v1/Locations/' + lati + ',' + longi + '?o=json&key=AnubGj32WoWIlLRjLyKZUHh3KNF5lhRwNT2TTEJ3p-E92TKDcgMFs4MY_aNHCsoe';
         $.getJSON(bingurl).then(function(data) {
           console.log(data.resourceSets[0].resources[0].address.countryRegion);
+          var fullTitle = $("title").text();
+          var truncateAt = fullTitle.indexOf(" (");
+          var trimmedString = fullTitle.substring(0, truncateAt);
+          console.log(trimmedString);
+
+          var RatingApiUrl = "http://0.tcp.ngrok.io:50427/rating/?name=" + trimmedString;
+
+          $.getJSON(RatingApiUrl).then(function(data) {
+            console.log("rating result");
+            console.log(data);
+          });
+
 
           // console.log(data.results[6].formatted_address);
 
           chrome.runtime.sendMessage({method: "getStatus"}, function(response) {
-            console.log(response.status);
+            // THIS IS THE LOCAL STORAGE AGE PARAM
+            // console.log(response.ageParam);
+
           });
 
         });
@@ -26,7 +38,18 @@ $(document).ready(function() {
   };
 
   locationFinder();
+  // console.log($("title").text);
+  // get_rating_id("Batman");
 });
+
+// var get_rating_id = function (name) {
+//     console.log('get rating called');
+//   var apiUrl = "http://api.kijkwijzer.nl/hackathon/suggest.php?term=" + name;
+//   console.log(apiUrl);
+//   $.getJSON(apiUrl).then(function(data) {
+//     console.log(data[0].value);
+//   });
+// };
 
 
 
