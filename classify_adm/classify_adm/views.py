fd50b70b4a6510b5045031b9b1a52ae4538d634a
oopa <eoogwe@gmail.com>
2015-10-20 02:05:56 +0100

    started work on api demo site

--- classify_adm/classify_adm/views.py
+++ classify_adm/classify_adm/views.py
@@ -3,13 +3,32 @@ __author__ = 'eoo'
 from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
 import requests
-
+import json
+from django.http import JsonResponse
 # Create your views here.
 
+def film_old_nojson(request):
+    name = request.GET['name']
+    result = get_rating(get_rating_id(name))
+    return HttpResponse(result)
 def film(request):
-	name = request.GET['name']
-	result = get_rating(get_rating_id(name))
-	return HttpResponse(result)
+    response = HttpResponse("",
+    content_type="application/json; charset=utf-8")
+
+    name = request.GET['name']
+    result = get_rating(get_rating_id(name))
+
+    response.write("{\"age\" : \"" + str(result) + "\"}");
+    return response;
+def film_2(request):
+    name = request.GET['name']
+    result = get_rating(get_rating_id(name))
+
+    data = {}
+    data['age'] = result
+    json_data = json.dumps(data)
+
+    return JsonResponse(json_data,safe=False)
 def get_rating_id(name):
 	url = "http://api.kijkwijzer.nl/hackathon/suggest.php?term=" + name
 	response = requests.get(url)
