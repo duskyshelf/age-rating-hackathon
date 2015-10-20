1ba4b11f1b94eca24e25d093584f24a44938ba51
oopa <eoogwe@gmail.com>
2015-10-20 12:27:31 +0100

    moved static files about

--- classify_adm/classify_adm/settings.py
+++ classify_adm/classify_adm/settings.py
@@ -101,3 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/home/eoo/Development/classify/api-demo-website"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/home/eoo/Development/classify/'
+)
